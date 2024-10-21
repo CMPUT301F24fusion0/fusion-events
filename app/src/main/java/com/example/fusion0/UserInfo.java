a37ef1a051ca9080c054b3d548dcc69a67850c4e
Sehej Brar <sehejpur@ualberta.ca>
2024-10-21 16:21:57 -0600

    Added required getters and setters plus a new parse function to remove extra values from phone numbers.

--- app/src/main/java/com/example/fusion0/UserInfo.java
+++ app/src/main/java/com/example/fusion0/UserInfo.java
@@ -1,5 +1,7 @@
 package com.example.fusion0;
 
+import java.util.ArrayList;
+
 public class UserInfo {
     String firstName, lastName, email, phoneNumber;
 
@@ -14,5 +16,46 @@ public class UserInfo {
         this.firstName = first;
         this.lastName = last;
         this.email = email;
+    }
+
+    private String parsed(String phoneNumber) {
+        for (char num: phoneNumber.toCharArray()) {
+            if (!Character.isDigit(num)) {
+                phoneNumber = phoneNumber.replace(String.valueOf(num), "");
+            }
+        }
+        return phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 }
