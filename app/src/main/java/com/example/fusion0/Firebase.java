07d049998faa44f06b7f3c59ae8c8bb920b6664c
Sehej Brar <sehejpur@ualberta.ca>
2024-10-21 22:35:15 -0600

    Allows search database by document ID (email) and returns the UserInfo object through a callback interface

--- app/src/main/java/com/example/fusion0/Firebase.java
+++ app/src/main/java/com/example/fusion0/Firebase.java
@@ -1,18 +1,17 @@
 package com.example.fusion0;
 
-import static java.security.AccessController.getContext;
-
-import android.widget.Toast;
-
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.auth.User;
 
 import java.util.HashMap;
 
 
 public class Firebase {
-    private FirebaseFirestore db;
+    // Adapted from https://stackoverflow.com/questions/48499310/how-to-return-a-documentsnapshot-as-a-result-of-a-method
+    public interface Callback {
+        void onCallback(UserInfo user);
+    }
+     private FirebaseFirestore db;
     private CollectionReference usersRef;
 
     public Firebase() {
@@ -22,17 +21,28 @@ public class Firebase {
 
     public void addUser(UserInfo userInfo) {
         HashMap<String, Object> user = userInfo.user();
-        String fullName = userInfo.getFirstName() + " " + userInfo.getLastName();
-        usersRef.add(user)
+        String email = userInfo.getEmail();
+        usersRef.document(email).set(user)
                 .addOnSuccessListener(documentReference -> {
                     System.out.println("Success");
                 })
-                .addOnFailureListener(documentReference -> {
-                    System.out.println("Failure" + documentReference.getMessage());
+                .addOnFailureListener(error -> {
+                    System.out.println("Failure" + error.getMessage());
                 });
     }
 
-    public void getUser() {
-
+    public void getUser(String email, Callback callback) {
+        usersRef.document(email).get()
+                .addOnSuccessListener(documentSnapshot -> {
+                    if (documentSnapshot.exists()) {
+                        UserInfo user = documentSnapshot.toObject(UserInfo.class);
+                        callback.onCallback(user);
+                    } else {
+                        System.out.println("not found");
+                    }
+                })
+                .addOnFailureListener(error -> {
+                    System.out.println("Failure" + error.getMessage());
+                });
     }
 }
