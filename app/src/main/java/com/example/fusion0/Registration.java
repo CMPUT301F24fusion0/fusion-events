47b526ce32c853d9f1ddcfcedcea0d125e9369ec
Sehej Brar <sehejpur@ualberta.ca>
2024-10-22 16:25:53 -0600

    Finished the registration class, new user is created when they first sign up for an event. Testing required

--- app/src/main/java/com/example/fusion0/Registration.java
+++ app/src/main/java/com/example/fusion0/Registration.java
@@ -1,7 +1,39 @@
 package com.example.fusion0;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 public class Registration extends Fragment {
+    EditText firstName, lastName, email, phoneNumber;
 
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        View view = inflater.inflate(R.layout.registration, container, false);
+        firstName = view.findViewById(R.id.firstName);
+        lastName = view.findViewById(R.id.lastName);
+        email = view.findViewById(R.id.email);
+        phoneNumber = view.findViewById(R.id.phone);
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        String first = firstName.getText().toString();
+        String last = lastName.getText().toString();
+        String emails = email.getText().toString();
+        String phone = phoneNumber.getText().toString();
+
+        new Firebase().addUser(new UserInfo(first, last, emails, phone));
+    }
 }
