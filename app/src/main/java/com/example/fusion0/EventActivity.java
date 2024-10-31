b34bafc053d2d5c3d78a41802f051b90ca46a9bb
Simon Haile <sahaile@ualberta.ca>
2024-10-31 17:18:11 -0600

    edited toolbar, activity_main.xml, mainactivity, androidmanifest (#75)
    * added xml files
    
    * added javadoc for waitlist class and resolved merge conflicts in waitlist.java (#70)
    
    * lottery and add entrant
    
    * javadoc for waitlist class
    
    * added offeranotherchance and removeEntrntFromWaitingList in Waitlist.java
    
    ---------
    
    Co-authored-by: Ali <aliabouei1@gmail.com>
    
    * Update .gitignore (#72)
    
    * Edit and Implement Waiting List and Lottery Functions for Event Management (#74)
    
    * lottery and add entrant
    
    * javadoc for waitlist class
    
    * added offeranotherchance and removeEntrntFromWaitingList in Waitlist.java
    
    * Updated EventInfo.java with latest changes
    
    ---------
    
    Co-authored-by: Ali <aliabouei1@gmail.com>
    
    * edited toolbar, activity_main.xml, mainactivity and androidmanifest
    
    ---------
    
    Co-authored-by: aliabouei <160547459+aliabouei@users.noreply.github.com>
    Co-authored-by: Ali <aliabouei1@gmail.com>
    Co-authored-by: Nimi Akinroye <125094797+nimithejeagerist@users.noreply.github.com>

--- app/src/main/java/com/example/fusion0/EventActivity.java
+++ app/src/main/java/com/example/fusion0/EventActivity.java
@@ -1,218 +1,197 @@
 package com.example.fusion0;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.HashMap;
-
-
-
-
-//import java.awt.Image;
-//import android.media.Image;
-
-
-import java.util.Date;
 import java.sql.Time;
-import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
-
-
-
-public class EventActivity {
-    private String organizer;
-    private String eventName;
-    private String address;
-    private String facilityName;
-    private Integer capacity;
-    //private Image eventPoster;
-    private float distance;
-    private Date startDate;
-    private Date endDate;
-    private Time startTime;
-    private Time endTime;
-    ArrayList<String> entrants;
-
-
-    public EventActivity(String organizer, String eventName, String address, String facilityName, Integer capacity, float distance, Date startDate, Date endDate, Time startTime, Time endTime, ArrayList<String> entrants) {
-        this.organizer = organizer;
-        this.eventName = eventName;
-        this.address = address;
-        this.facilityName = facilityName;
-        this.capacity = capacity;
-        this.distance = distance;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.startTime = startTime;
-        this.endTime = endTime;
-        this.entrants = entrants;
+public class EventActivity extends AppCompatActivity{
+    private EditText eventName;
+    private EditText description;
+    private Calendar startDateCalendar;
+    private TextView dateRequirementsTextView;
+    private TextView startDateTextView;
+    private TextView startTimeTextView;
+    private TextView endDateTextView;
+    private TextView endTimeTextView;
+    private EditText capacity;
+    private Button addButton;
+    private Button exitButton;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_event);
+        eventName = findViewById(R.id.EventName);
+        description = findViewById(R.id.Description);
+        dateRequirementsTextView = findViewById(R.id.date_requirements_text);
+        startDateTextView = findViewById(R.id.start_date_text);
+        startTimeTextView = findViewById(R.id.start_time_text);
+        endDateTextView = findViewById(R.id.end_date_text);
+        endTimeTextView = findViewById(R.id.end_time_text);
+        capacity = findViewById(R.id.Capacity);
+        addButton = findViewById(R.id.add_button);
+        exitButton = findViewById(R.id.exit_button);
+        StartDateButtonHandling();
+        EndDateButtonHandling();
+        AddEvent();
+        ExitButtonHandling();
     }
-
-
-    public HashMap<String, Object> event() {
-        HashMap<String, Object> event = new HashMap<>();
-
-
-        event.put("organizer", this.organizer);
-        event.put("eventName", this.eventName);
-        event.put("address", this.address);
-        event.put("facilityName", this.facilityName);
-        event.put("capacity", this.capacity);
-        event.put("distance", this.distance);
-        event.put("startDate", this.startDate);
-        event.put("endDate", this.endDate);
-        event.put("startTime", this.startTime);
-        event.put("endTime", this.endTime);
-        event.put("entrants", this.entrants);
-
-
-        return event;
+    private void StartDateButtonHandling() {
+        Button startDateButton = findViewById(R.id.start_date_button);
+        TextView startDateTextView = findViewById(R.id.start_date_text);
+        TextView startTimeTextView = findViewById(R.id.start_time_text);
+        startDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(com.example.fusion0.EventActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        startDateCalendar = Calendar.getInstance();
+                        startDateCalendar.set(selectedYear, selectedMonth, selectedDay);
+                        Calendar currentDate = Calendar.getInstance();
+                        if (startDateCalendar.before(currentDate)) {
+                            dateRequirementsTextView.setText("Date Must Be Today or Later.");
+                            dateRequirementsTextView.setVisibility(View.VISIBLE);
+                            startDateTextView.setVisibility(View.INVISIBLE);
+                            startDateCalendar = null;
+                        } else {
+                            String selectedDate = String.format(Locale.US, "%d/%d/%d", selectedMonth + 1, selectedDay, selectedYear);
+                            startDateTextView.setText(selectedDate);
+                            startDateTextView.setVisibility(View.VISIBLE);
+                            dateRequirementsTextView.setVisibility(View.INVISIBLE);
+                            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                            int minute = calendar.get(Calendar.MINUTE);
+                            TimePickerDialog timePickerDialog = new TimePickerDialog(com.example.fusion0.EventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                                    startDateCalendar.set(Calendar.HOUR_OF_DAY, selectedHour);
+                                    startDateCalendar.set(Calendar.MINUTE, selectedMinute);
+                                    // Check if start time is before current time
+                                    Calendar currentTime = Calendar.getInstance();
+                                    if (startDateCalendar.before(currentTime)) {
+                                        dateRequirementsTextView.setText("Start Time Must Be Now or Later.");
+                                        dateRequirementsTextView.setVisibility(View.VISIBLE);
+                                        startTimeTextView.setVisibility(View.INVISIBLE);
+                                    } else {
+                                        String selectedTime = String.format(Locale.US, "%02d:%02d", selectedHour, selectedMinute);
+                                        startTimeTextView.setText(selectedTime);
+                                        startTimeTextView.setVisibility(View.VISIBLE);
+                                        dateRequirementsTextView.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                            }, hour, minute, true);
+                            timePickerDialog.show();
+                        }
+                    }
+                }, year, month, day);
+                dialog.show();
+            }
+        });
     }
-
-
-    public String getOrganizer() {
-        return organizer;
+    private void EndDateButtonHandling() {
+        Button endDateButton = findViewById(R.id.end_date_button);
+        TextView endDateTextView = findViewById(R.id.end_date_text);
+        TextView endTimeTextView = findViewById(R.id.end_time_text);
+        // End Date Button
+        endDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+                DatePickerDialog dialog = new DatePickerDialog(com.example.fusion0.EventActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                        Calendar endDateCalendar = Calendar.getInstance();
+                        endDateCalendar.set(selectedYear, selectedMonth, selectedDay);
+                        // Copy time from startDateCalendar if the dates are the same
+                        if (startDateCalendar != null &&
+                                endDateCalendar.get(Calendar.YEAR) == startDateCalendar.get(Calendar.YEAR) &&
+                                endDateCalendar.get(Calendar.MONTH) == startDateCalendar.get(Calendar.MONTH) &&
+                                endDateCalendar.get(Calendar.DAY_OF_MONTH) == startDateCalendar.get(Calendar.DAY_OF_MONTH)) {
+                            // Set the end time to the start time
+                            endDateCalendar.set(Calendar.HOUR_OF_DAY, startDateCalendar.get(Calendar.HOUR_OF_DAY));
+                            endDateCalendar.set(Calendar.MINUTE, startDateCalendar.get(Calendar.MINUTE));
+                        }
+                        Calendar currentDate = Calendar.getInstance();
+                        if (startDateCalendar == null) {
+                            dateRequirementsTextView.setText("Please select a Start Date first.");
+                            dateRequirementsTextView.setVisibility(View.VISIBLE);
+                            endDateTextView.setVisibility(View.INVISIBLE);
+                            endTimeTextView.setVisibility(View.INVISIBLE);
+                        } else if (endDateCalendar.before(currentDate)) {
+                            dateRequirementsTextView.setText("End Date Must Be Today or Later.");
+                            dateRequirementsTextView.setVisibility(View.VISIBLE);
+                            endDateTextView.setVisibility(View.INVISIBLE);
+                            endTimeTextView.setVisibility(View.INVISIBLE);
+                        } else if (endDateCalendar.before(startDateCalendar)) {
+                            endDateTextView.setVisibility(View.INVISIBLE);
+                            endTimeTextView.setVisibility(View.INVISIBLE);
+                            dateRequirementsTextView.setText("End Date Must Be On or After Start Date.");
+                            dateRequirementsTextView.setVisibility(View.VISIBLE);
+                        } else {
+                            String selectedDate = String.format(Locale.US, "%d/%d/%d", selectedMonth + 1, selectedDay, selectedYear);
+                            endDateTextView.setText(selectedDate);
+                            endDateTextView.setVisibility(View.VISIBLE);
+                            dateRequirementsTextView.setVisibility(View.INVISIBLE);
+                            int hour = calendar.get(Calendar.HOUR_OF_DAY);
+                            int minute = calendar.get(Calendar.MINUTE);
+                            TimePickerDialog timePickerDialog = new TimePickerDialog(com.example.fusion0.EventActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                                @Override
+                                public void onTimeSet(TimePicker view, int selectedHour, int selectedMinute) {
+                                    endDateCalendar.set(Calendar.HOUR_OF_DAY, selectedHour);
+                                    endDateCalendar.set(Calendar.MINUTE, selectedMinute);
+                                    // Check if end time is after or equal to start time
+                                    if (endDateCalendar.before(startDateCalendar)) {
+                                        dateRequirementsTextView.setText("End Time Must Be After Start Time.");
+                                        dateRequirementsTextView.setVisibility(View.VISIBLE);
+                                        endTimeTextView.setVisibility(View.INVISIBLE);
+                                    } else {
+                                        String selectedTime = String.format(Locale.US, "%02d:%02d", selectedHour, selectedMinute);
+                                        endTimeTextView.setText(selectedTime);
+                                        endTimeTextView.setVisibility(View.VISIBLE);
+                                        dateRequirementsTextView.setVisibility(View.INVISIBLE);
+                                    }
+                                }
+                            }, hour, minute, true);
+                            timePickerDialog.show();
+                        }
+                    }
+                }, year, month, day);
+                dialog.show();
+            }
+        });
     }
-
-
-    public void setOrganizer(String organizer) {
-        this.organizer = organizer;
+    private void setDateRequirements(String message, TextView textView, boolean hideOtherTextViews) {
+        dateRequirementsTextView.setText(message);
+        dateRequirementsTextView.setVisibility(View.VISIBLE);
+        textView.setVisibility(View.INVISIBLE);
+        if (hideOtherTextViews) {
+            startTimeTextView.setVisibility(View.INVISIBLE);
+            endTimeTextView.setVisibility(View.INVISIBLE);
+        }
     }
-
-
-    public String getEventName() {
-
-
-        return eventName;
+    private void AddEvent(){
     }
-
-
-    public void setEventName(String eventName) {
-
-
-        this.eventName = eventName;
+    private void ExitButtonHandling() {
+        exitButton.setOnClickListener(v -> finish());
     }
-
-
-    public String getAddress() {
-
-
-        return address;
-    }
-
-
-    public void setAddress(String address) {
-
-
-        this.address = address;
-    }
-
-
-    public String getFacilityName() {
-
-
-        return facilityName;
-    }
-
-
-    public void setFacilityName(String facilityName) {
-
-
-        this.facilityName = facilityName;
-    }
-
-
-    public Integer getCapacity() {
-
-
-        return capacity;
-    }
-
-
-    public void setCapacity(Integer capacity) {
-
-
-        this.capacity = capacity;
-    }
-
-
-    public float getDistance() {
-
-
-        return distance;
-    }
-
-
-    public void setDistance(float distance) {
-
-
-        this.distance = distance;
-    }
-
-
-    public Date getStartDate() {
-
-
-        return startDate;
-    }
-
-
-    public void setStartDate(Date startDate) {
-
-
-        this.startDate = startDate;
-    }
-
-
-    public Date getEndDate() {
-
-
-        return endDate;
-    }
-
-
-    public void setEndDate(Date endDate) {
-
-
-        this.endDate = endDate;
-    }
-
-
-    public Time getStartTime() {
-
-
-        return startTime;
-    }
-    public void setStartTime(Time startTime) {
-
-
-        this.startTime = startTime;
-    }
-
-
-    public Time getEndTime() {
-
-
-        return endTime;
-    }
-
-
-    public void setEndTime(Time endTime) {
-
-
-        this.endTime = endTime;
-    }
-
-
-
-
-    public ArrayList<String> getEntrants() {
-        return entrants;
-    }
-
-
-    public void setEntrants(ArrayList<String> entrants) {
-        this.entrants = entrants;
-    }
-
-
 }
-
