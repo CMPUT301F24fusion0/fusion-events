f8f70c8e39b9f5162e8a7bddf46c380c739354fa
sahaile <sahaile@ualberta.ca>
2024-10-31 19:00:51 -0600

    finished image uploading and autocomplete address

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,7 +48,6 @@ dependencies {
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
-    implementation("me.dm7.barcodescanner:zxing:1.9.8")
     implementation ("com.google.zxing:core:3.4.1")
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-firestore")
