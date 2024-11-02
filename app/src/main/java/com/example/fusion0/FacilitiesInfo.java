b191af245991212f5b6c74e0c4258163b7bd206f
sahaile <sahaile@ualberta.ca>
2024-11-02 11:50:25 -0600

    Added facility handling, image handling to eventactivity

--- app/src/main/java/com/example/fusion0/FacilitiesInfo.java
+++ app/src/main/java/com/example/fusion0/FacilitiesInfo.java
@@ -1,8 +1,10 @@
 package com.example.fusion0;
 
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.UUID;
+
 
 public class FacilitiesInfo {
     public String facilityID;
@@ -10,63 +12,66 @@ public class FacilitiesInfo {
     public String facilityName;
     public String owner;
     ArrayList<String> events;
-    EventFirebase firebase;
 
-    public FacilitiesInfo() {
-        this.firebase = new EventFirebase();
-    }
 
-    public FacilitiesInfo(String address, String facilityName, String owner) {
+    public FacilitiesInfo(String address, String facilityName, String owner){
         this.address = address;
         this.facilityName = facilityName;
         this.owner = owner;
         this.events = new ArrayList<>();
         this.facilityID = UUID.randomUUID().toString();
-        this.firebase = new EventFirebase();
     }
 
-    public HashMap<String, Object> facility() {
+
+    public HashMap<String,Object> facility() {
         HashMap<String, Object> facility = new HashMap<>();
 
-        facility.put("address", this.address);
-        facility.put("facilityName", this.facilityName);
-        facility.put("owner", this.owner);
-        facility.put("events", this.events);
+
+        facility().put("address", this.address);
+        facility().put("facilityName", this.facilityName);
+        facility().put("owner", this.owner);
+        facility().put("events", this.events);
+
 
         return facility;
     }
-
-    public String getFacilityID() {
+    public String getFacilityID(){
         return facilityID;
     }
-
     public String getFacilityName() {
         return facilityName;
     }
+
 
     public void setFacilityName(String facilityName) {
         this.facilityName = facilityName;
     }
 
+
     public String getAddress() {
         return address;
     }
+
 
     public void setAddress(String address) {
         this.address = address;
     }
 
+
     public String getOwner() {
         return owner;
     }
+
 
     public void setOwner(String owner) {
         this.owner = owner;
     }
 
+
     public ArrayList<String> getEvents() {
         return events;
     }
+
 
     public void setEvents(ArrayList<String> events) {
         this.events = events;
