99d317357061c50bf8bc2bde25add9fa8fa49d2a
Sehej Brar <sehejpur@ualberta.ca>
2024-11-02 12:21:06 -0600

    Added JavaDocs for the EventFirebase class

--- app/src/main/java/com/example/fusion0/EventFirebase.java
+++ app/src/main/java/com/example/fusion0/EventFirebase.java
@@ -18,6 +18,9 @@ public class EventFirebase {
     private final CollectionReference eventsRef;
 
 
+    /**
+     * Constructor for the EventFirebase class
+     */
     public EventFirebase() {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         organizersRef = db.collection("organizers");
@@ -25,6 +28,10 @@ public class EventFirebase {
         eventsRef = db.collection("facilities");
     }
 
+    /**
+     * Adds an organizer to the firebase
+     * @param organizerInfo a class that contains information about the organizer
+     */
     public void addOrganizer(OrganizerInfo organizerInfo){
         HashMap<String, Object> organizer = organizerInfo.organizer();
         String deviceId = organizerInfo.getDeviceId();
@@ -37,6 +44,11 @@ public class EventFirebase {
                 });
     }
 
+    /**
+     * Allows for the editing of the organizer through the organizerInfo class
+     * @param organizerInfo the class that contains the organizer information
+     * @param updatedData the hashmap that contains edited information about the organizer
+     */
     public void editOrganizer(OrganizerInfo organizerInfo, HashMap<String, Object> updatedData) {
         String deviceId = organizerInfo.getDeviceId();
         organizersRef.document(deviceId).set(updatedData, SetOptions.merge())
@@ -48,6 +60,10 @@ public class EventFirebase {
                 });
     }
 
+    /**
+     * This function allows for the deletion of an organizer based on their device id
+     * @param deviceId a string containing the device id
+     */
     public void deleteOrganizer(String deviceId){
         organizersRef.document(deviceId).delete().addOnSuccessListener(documentReference -> {
             System.out.println("Success");
@@ -56,6 +72,10 @@ public class EventFirebase {
         });
     }
 
+    /**
+     * Add a facility based on the FacilitiesInfo class
+     * @param facilitiesInfo the class containing the facility information
+     */
     public void addFacility(FacilitiesInfo facilitiesInfo){
         HashMap<String, Object> facility = facilitiesInfo.facility();
         String facilityID = facilitiesInfo.getFacilityID();
@@ -68,6 +88,11 @@ public class EventFirebase {
                 });
     }
 
+    /**
+     * This function allows for the editing of a facility using the information and the hashmap
+     * @param facilitiesInfo the class containing the information about the facility
+     * @param updatedData the hashmap with the edited information
+     */
     public void editFacility(FacilitiesInfo facilitiesInfo, HashMap<String, Object> updatedData){
         String facilityID = facilitiesInfo.getFacilityID();
         facilitiesRef.document(facilityID).set(updatedData, SetOptions.merge())
@@ -79,6 +104,10 @@ public class EventFirebase {
                 });
     }
 
+    /**
+     * Deletes a facility based on the facilityID
+     * @param facilityID a random ID generated in the facility class
+     */
     public void deleteFacility(String facilityID){
         facilitiesRef.document(facilityID).delete().addOnSuccessListener(documentReference -> {
             System.out.println("Success");
@@ -89,6 +118,10 @@ public class EventFirebase {
 
     }
 
+    /**
+     * Adds a new event to the events collection using the event ID
+     * @param eventInfo a class containing the eventInfo information
+     */
     public void addEvent(EventInfo eventInfo){
         HashMap<String, Object> event = eventInfo.event();
         String eventID = eventInfo.getEventID();
@@ -99,6 +132,11 @@ public class EventFirebase {
         });
     }
 
+    /**
+     * Edits an event using the EventInfo class and a hashmap with the edited information
+     * @param eventInfo the class containing all the event information
+     * @param updatedData the hashmap with the new edited information
+     */
     public void editEvent(EventInfo eventInfo,HashMap<String, Object> updatedData){
         String eventID = eventInfo.getEventID();
         eventsRef.document(eventID).set(updatedData, SetOptions.merge())
@@ -110,6 +148,10 @@ public class EventFirebase {
                 });
     }
 
+    /**
+     * Allows for the deletion of an event using the event ID
+     * @param eventID the randomly generated eventID generated in the EventInfo class
+     */
     public void deleteEvent(String eventID){
         eventsRef.document(eventID).delete().addOnSuccessListener(documentReference -> {
             System.out.println("Success");
