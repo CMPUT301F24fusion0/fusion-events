32ebdbd3d1172af656bc995f9887ad6923d8ae92
sahaile <sahaile@ualberta.ca>
2024-11-04 14:39:28 -0700

    added view event activity and qr and image display

--- app/src/main/java/com/example/fusion0/FavouriteActivity.java
+++ app/src/main/java/com/example/fusion0/FavouriteActivity.java
@@ -165,6 +165,7 @@ public class FavouriteActivity extends AppCompatActivity {
                 String eventName = organizer.getEventsNames().get(position);
 
                 Intent intent = new Intent(FavouriteActivity.this, ViewEventActivity.class);
+                intent.putExtra("eventName", eventName);
                 intent.putExtra("eventID", eventID);
                 startActivity(intent);
             });
