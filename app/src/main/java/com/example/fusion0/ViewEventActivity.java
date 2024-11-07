33fd868acb852e39509ba303f8fecb1a56ef50e6
sahaile <sahaile@ualberta.ca>
2024-11-07 14:04:21 -0700

    added handling of null varaibles when adding event, changed eventinfo and facilities info classes to handle longitude, latitude, geolocation and waitinglist, edited event_view.xml not finished

--- app/src/main/java/com/example/fusion0/ViewEventActivity.java
+++ app/src/main/java/com/example/fusion0/ViewEventActivity.java
@@ -1,8 +1,8 @@
 package com.example.fusion0;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.util.Log;
@@ -17,20 +17,23 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.zxing.WriterException;
 
-import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
 
 public class ViewEventActivity extends AppCompatActivity {
     private String deviceID;
     private Boolean isOwner = false;
     private Spinner eventFacility;
-    private TextView eventNameTextView, eventDescriptionTextView, eventStartDateTextView, eventEndDateTextView, eventCapacityTextView;
+    private TextView eventNameTextView, eventDescriptionTextView, eventStartDateTextView, eventEndDateTextView, eventCapacityTextView, waitinglistFullTextView;
     private EditText eventNameEditText, eventDescriptionEditText, eventCapacityEditText;
     private ImageView eventPosterImageView, qrImageView;
     private ListView entrantsListView, chosenEntrantsListView, cancelledEntrantsListView;
@@ -39,12 +42,19 @@ public class ViewEventActivity extends AppCompatActivity {
     private EventInfo event;
     private LinearLayout toolbar;
 
+
+    private FusedLocationProviderClient fusedLocationClient;
+    private Double longitude = null;
+    private Double latitude = null;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_view);
 
         deviceID = Settings.Secure.getString(getContentResolver(), Settings.Secure.ANDROID_ID);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
 
 
         backButton = findViewById(R.id.backButton);
@@ -53,7 +63,7 @@ public class ViewEventActivity extends AppCompatActivity {
         eventFacility= findViewById(R.id.spinner_facilities);
         eventStartDateTextView = findViewById(R.id.start_date_text);
         eventEndDateTextView = findViewById(R.id.end_date_text);
-        eventCapacityTextView = findViewById(R.id.Capacity);
+        eventCapacityTextView = findViewById(R.id.editCapacity);
         eventNameEditText = findViewById(R.id.editEventName);
         eventDescriptionEditText = findViewById(R.id.Description);
         eventPosterImageView = findViewById(R.id.uploaded_image_view);
@@ -68,6 +78,7 @@ public class ViewEventActivity extends AppCompatActivity {
         joinButton = findViewById(R.id.join_button);
         cancelButton = findViewById(R.id.cancel_button);
         saveButton = findViewById(R.id.save_button);
+        waitinglistFullTextView = findViewById(R.id.waitinglist_full_text_view);
 
         toolbar = findViewById(R.id.toolbar);
 
@@ -113,12 +124,18 @@ public class ViewEventActivity extends AppCompatActivity {
                         if (deviceID.equals(event.getOrganizer())) {
                             isOwner = true;
                         }else{
-                            joinButton.setVisibility(View.VISIBLE);
                             editButton.setVisibility(View.GONE);
                             deleteButton.setVisibility(View.GONE);
                             cancelButton.setVisibility(View.GONE);
                             saveButton.setVisibility(View.GONE);
 
+                            ArrayList<String> currentEntrants = event.getWaitinglist();
+                            int capacity = Integer.parseInt(event.getCapacity());
+                            if (currentEntrants.size() < capacity) {
+                                joinButton.setVisibility(View.VISIBLE);
+                            }else{
+                                waitinglistFullTextView.setVisibility(View.VISIBLE);
+                            }
                         }
                     }
                 }
@@ -141,13 +158,35 @@ public class ViewEventActivity extends AppCompatActivity {
         });
 
         joinButton.setOnClickListener(view ->{
-            ArrayList<String> currentEntrants = event.getEntrants();
-            currentEntrants.add(deviceID);
-            event.setEntrants(currentEntrants);
+            if (event.getGeolocation()){
+                getCurrentLocation();
+            }
+            ArrayList<String> currentEntrants = event.getWaitinglist();
+            String newEntrant = "[" + deviceID + ", " + latitude + ", " + longitude + "]";
+            currentEntrants.add(newEntrant);
+            event.setWaitinglist(currentEntrants);
             EventFirebase.editEvent(event);
-            Toast.makeText(ViewEventActivity.this, "Joined Successfully.", Toast.LENGTH_SHORT).show();
-
+            Toast.makeText(ViewEventActivity.this, "Joined Waiting List Successfully.", Toast.LENGTH_SHORT).show();
         });
     }
-}
 
+    private void getCurrentLocation() {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            resultLauncher.launch(android.Manifest.permission.ACCESS_FINE_LOCATION);
+            return;
+        }
+
+        fusedLocationClient.getLastLocation().addOnSuccessListener(this, location -> {
+            if (location != null) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+            }
+        });
+    }
+
+    private final ActivityResultLauncher<String> resultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), isGranted -> {
+        if (isGranted) {
+            getCurrentLocation();
+        }
+    });
+}
