23eb6c23eb8e7bc4af899c4fcfe8d5c19a21e0e8
sahaile <sahaile@ualberta.ca>
2024-11-21 13:08:33 -0700

    created fragments for list view

---
+++ app/src/main/java/com/example/fusion0/ChosenEntrants.java
@@ -0,0 +1,58 @@
+package com.example.fusion0;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ChosenEntrants extends Fragment {
+    ImageButton backButton;
+    TextView chosenEntrantsCapacityRatio;
+    ListView chosenEntrantsListView;
+    Button lotteryButton;
+    EventFirebase firebase;
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.chosen_entrants, container, false);
+
+        backButton = view.findViewById(R.id.backButton);
+        chosenEntrantsCapacityRatio = view.findViewById(R.id.ratio);
+        chosenEntrantsListView = view.findViewById(R.id.chosenEntrantsListView);
+        lotteryButton = view.findViewById(R.id.fill_lottery_button);
+        firebase = new EventFirebase();
+
+        List<UserInfo> chosenEntrants = new ArrayList<>();
+
+        ProfileListAdapter adapter = new ProfileListAdapter(getContext(), chosenEntrants);
+        chosenEntrantsListView.setAdapter(adapter);
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(getActivity(), MainActivity.class);
+            startActivity(intent);
+        });
+
+        lotteryButton.setOnClickListener(v -> {
+        });
+    }
+}
+
