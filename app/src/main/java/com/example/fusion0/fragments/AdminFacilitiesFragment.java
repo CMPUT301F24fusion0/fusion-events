714095a8e47ec8e2aa31e11d82ea518466ed5dcd
malshaanm <mkodi@Malshaans-MBP.ed.shawcable.net>
2024-11-24 16:04:59 -0700

    added admin facilities fragment

---
+++ app/src/main/java/com/example/fusion0/fragments/AdminFacilitiesFragment.java
@@ -0,0 +1,13 @@
+package com.example.fusion0.fragments;
+
+import androidx.fragment.app.Fragment;
+
+/**
+ * @author Malshaan Kodithuwakku
+ * This fragment allows admins to browse through all facilities that have events.
+ * Admins can view the facilities and delete them if they violate app policy.
+ */
+public class AdminFacilitiesFragment extends Fragment {
+
+
+}
