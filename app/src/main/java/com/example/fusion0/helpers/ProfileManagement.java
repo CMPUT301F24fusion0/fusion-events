efc3c18f114ff05ec5dbc89b205ec69f5d2565ef
Sehej Brar <sehejpur@ualberta.ca>
2024-11-26 15:04:03 -0700

    fixed so that admin features is only visible for admins

--- app/src/main/java/com/example/fusion0/helpers/ProfileManagement.java
+++ app/src/main/java/com/example/fusion0/helpers/ProfileManagement.java
@@ -1,5 +1,7 @@
 package com.example.fusion0.helpers;
 
+import android.util.Log;
+
 import com.example.fusion0.models.UserInfo;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.CollectionReference;
