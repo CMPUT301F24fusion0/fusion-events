d8f8dbbde81e624d333dfe821c291078be115896
Nimi Akinroye <125094797+nimithejeagerist@users.noreply.github.com>
2024-11-30 12:21:03 -0700

    ViewEventFragment UI done (#167)
    * If applied, I would have made significant changes to the way we create deterministic images
    
    * Changes made in manifest
    
    * Little changes to EventFragment
    
    * Everything works except going from QRFragment to ViewEventActivity and I will fix it later today
    
    * A space
    
    * Turning actvities to fragment
    
    * Updated changes to favourite fragment
    
    * Changes to FavouriteFragment
    
    * Changed the entire toolbar, on to working on individual fragments
    
    * Began skeleton layouts for loading
    
    * Profile fragment is done, need to create a class for populating the event stats
    
    * Setting up work in AddEventFragment
    
    * AddEventFragment done
    
    * Began working on favourite fragment
    
    * FavouriteFragment design done
    
    * ViewEventaFragment fixed
    
    * Merge done
    
    * Bugs and fixes

--- app/src/main/java/com/example/fusion0/activities/MainActivity.java
+++ app/src/main/java/com/example/fusion0/activities/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.fusion0.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.example.fusion0.R;
 
@@ -12,6 +15,7 @@ import com.example.fusion0.R;
  * and directs users to the Profile page, initializing Firebase and LoginManagement to handle user sessions.
  */
 public class MainActivity extends AppCompatActivity {
+
     /**
      * Initializes the MainActivity and manages user session and state.
      * Sets up Firebase, handles login state, and initializes the profile button to access the ProfileFragment.
@@ -24,5 +28,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         EdgeToEdge.enable(this);
+
+        View rootLayout = findViewById(R.id.rootLayout);
+
+        ViewCompat.setOnApplyWindowInsetsListener(rootLayout, (view, windowInsetsCompat) -> {
+            // Get system bar insets using WindowInsetsCompat
+            androidx.core.graphics.Insets systemBarInsets =
+                    windowInsetsCompat.getInsets(WindowInsetsCompat.Type.systemBars());
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                android.graphics.Insets platformInsets = systemBarInsets.toPlatformInsets();
+
+                view.setPadding(
+                        platformInsets.left,
+                        platformInsets.top,
+                        platformInsets.right,
+                        0
+                );
+            } else {
+                view.setPadding(
+                        systemBarInsets.left,
+                        systemBarInsets.top,
+                        systemBarInsets.right,
+                        0
+                );
+            }
+
+            return WindowInsetsCompat.CONSUMED;
+        });
+
     }
 }
