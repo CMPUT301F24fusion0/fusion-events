7f55ac3139ede5055c01c65e313261f359d654b5
Sehej Brar <150099256+sehejb@users.noreply.github.com>
2024-11-29 18:43:45 -0700

    Update TestFragmentActivity.java

--- app/src/main/java/com/example/fusion0/fragments/TestFragmentActivity.java
+++ app/src/main/java/com/example/fusion0/fragments/TestFragmentActivity.java
@@ -3,5 +3,4 @@ package com.example.fusion0.fragments;
 import androidx.fragment.app.FragmentActivity;
 
 public class TestFragmentActivity extends FragmentActivity {
-    // Add any necessary setup or empty implementation for testing.
 }
