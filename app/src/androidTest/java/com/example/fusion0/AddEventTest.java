a1e604f78c1f89c7a15c07b7a487963eebf47829
Sehej Brar <sehejpur@ualberta.ca>
2024-12-01 00:44:29 -0700

    1/2 addevent tests working

--- app/src/androidTest/java/com/example/fusion0/AddEventTest.java
+++ app/src/androidTest/java/com/example/fusion0/AddEventTest.java
@@ -3,10 +3,7 @@ package com.example.fusion0;
 import static androidx.test.espresso.action.ViewActions.click;
 import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.replaceText;
-import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
-
-import android.widget.DatePicker;
 
 import androidx.test.espresso.Espresso;
 import androidx.test.espresso.assertion.ViewAssertions;
@@ -32,8 +29,8 @@ public class AddEventTest {
      */
     @Test
     public void testGoAddEvent() {
-        Espresso.onView(withId(R.id.toolbar_favourite)).perform(click());
-        Espresso.onView(withId(R.id.createEventHeader)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
+        Espresso.onView(withId(R.id.toolbar_add)).perform(click());
+        Espresso.onView(withId(R.id.header_text)).check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
 
     /**
@@ -42,7 +39,7 @@ public class AddEventTest {
      */
     @Test
     public void fillOutForm() {
-        Espresso.onView(withId(R.id.toolbar_favourite)).perform(click());
+        Espresso.onView(withId(R.id.toolbar_add)).perform(click());
         Espresso.onView(withId(R.id.EventName)).perform(replaceText("Event Name"), closeSoftKeyboard());
         Espresso.onView(withId(R.id.Description)).perform(replaceText("Event Description"), closeSoftKeyboard());
         Espresso.onView(withId(R.id.save_button)).perform(click());
