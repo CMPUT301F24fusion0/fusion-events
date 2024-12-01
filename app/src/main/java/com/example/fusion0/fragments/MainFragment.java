f04234a63d987a6ee68f5d63f238fac17e483b67
Sehej Brar <sehejpur@ualberta.ca>
2024-11-30 19:11:10 -0700

    changes

--- app/src/main/java/com/example/fusion0/fragments/MainFragment.java
+++ app/src/main/java/com/example/fusion0/fragments/MainFragment.java
@@ -1,13 +1,10 @@
 package com.example.fusion0.fragments;
 
-import static android.content.ContentValues.TAG;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.media.Image;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
