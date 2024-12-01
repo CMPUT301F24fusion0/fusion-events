a333b2fbbf35dba94a30557d4acb3418277de4c0
Sehej Brar <sehejpur@ualberta.ca>
2024-12-01 01:24:42 -0700

    simpletextwatcher and aevm javadocs

--- app/src/main/java/com/example/fusion0/models/AddEventViewModel.java
+++ app/src/main/java/com/example/fusion0/models/AddEventViewModel.java
@@ -4,18 +4,33 @@ import androidx.lifecycle.ViewModel;
 
 import com.example.fusion0.helpers.AddEventHelper;
 
+/**
+ * ViewModel for AddEventHelper for adding events
+ * @author Nimi Akinroye
+ */
 public class AddEventViewModel extends ViewModel {
     private AddEventHelper helper;
 
-    // Initialize helper with default values
+    /**
+     * Initialize with default values
+     * @param helper the helper object to initialize
+     */
     public void initializeHelper(AddEventHelper helper) {
         this.helper = helper;
     }
 
+    /**
+     * Gets helper
+     * @return the helper
+     */
     public AddEventHelper getHelper() {
         return helper;
     }
 
+    /**
+     * Updates helper
+     * @param newHelper helper to change
+     */
     public void updateHelper(AddEventHelper newHelper) {
         this.helper = newHelper;
     }
--- app/src/main/java/com/example/fusion0/models/SimpleTextWatcher.java
+++ app/src/main/java/com/example/fusion0/models/SimpleTextWatcher.java
@@ -3,17 +3,38 @@ package com.example.fusion0.models;
 import android.text.Editable;
 import android.text.TextWatcher;
 
+/**
+ * An implementation of {@link TextWatcher}
+ * @author Nimi Akinroye
+ */
 public abstract class SimpleTextWatcher implements TextWatcher {
+    /**
+     * Called before text-changed
+     * @param s char sequence being modified
+     * @param start where to start
+     * @param count number of characters to be replaced
+     * @param after number of characters to replace with
+     */
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
         // No-op
     }
 
+    /**
+     * Called when text is being changed
+     * @param s char sequence being modified
+     * @param start where to start
+     * @param count number of characters to be replaced
+     */
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
         // No-op
     }
 
+    /**
+     * Called after the text has been changed
+     * @param s text after the change
+     */
     @Override
     public abstract void afterTextChanged(Editable s);
 }
