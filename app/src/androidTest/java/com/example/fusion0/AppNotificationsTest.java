0f712e40fc713ae9cb6d2f8072a4b3aa94e20ba2
Sehej Brar <sehejpur@ualberta.ca>
2024-12-01 17:23:24 -0700

    added javadocs

--- app/src/androidTest/java/com/example/fusion0/AppNotificationsTest.java
+++ app/src/androidTest/java/com/example/fusion0/AppNotificationsTest.java
@@ -8,6 +8,7 @@ import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import org.junit.After;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
